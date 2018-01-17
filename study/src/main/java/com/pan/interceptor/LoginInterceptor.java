53e3a5d16300d0c035b98f61cb6188d2ab15b675
panzhigao-wb <panzhigao-wb@010A1507240069.58ganji-corp.com>
2018-01-17 17:24:52 +0800

    修改

--- study/src/main/java/com/pan/interceptor/LoginInterceptor.java
+++ study/src/main/java/com/pan/interceptor/LoginInterceptor.java
@@ -7,9 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.pan.entity.User;
-import com.pan.service.impl.UserServiceImpl;
 import com.pan.util.CookieUtils;
 
 /**
